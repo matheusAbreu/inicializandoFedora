285c2bb1c81dc5d80777fbc66a7a8d508adda32e
Abreu <matheus.moon@gmail.com>
2020-07-29 21:54:16 -0300

    Comentando a instalação dos drives da nvidia

--- installer.sh
+++ installer.sh
@@ -20,7 +20,7 @@ rpm --import https://packages.microsoft.com/keys/microsoft.asc;
 sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo';
 
 #nvidia(negative 17)
-dnf config-manager --add-repo=https://negativo17.org/repos/fedora-nvidia.repo;
+#dnf config-manager --add-repo=https://negativo17.org/repos/fedora-nvidia.repo;
 
 dnf upgrade -yq;
 
@@ -30,8 +30,8 @@ flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flat
 #install programs
 
 #nivida driver
-echo 'Instalando drivers Nvidia';
-dnf install -yq nvidia-driver nvidia-driver-libs.i686 nvidia-settings akmod-nvidia cuda nvidia-driver-cuda --allowerasing --best ;
+#echo 'Instalando drivers Nvidia';
+#dnf install -yq nvidia-driver nvidia-driver-libs.i686 nvidia-settings akmod-nvidia cuda nvidia-driver-cuda --allowerasing --best ;
 
 echo 'Instalando drivers PostgreSQL Server 12';
 dnf groupinstall -yq 'PostgreSQL Database Server 12 PGDG' --with-optional;
